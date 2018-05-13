0ee5e69debafc601948c6966e1c8f6216a48ac8d
TBargat <tbargat@gmail.com>
2018-05-13 17:55:45 +0200

    Initial Commit

---
+++ The Game Of Heroes - Bargat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Game Of Heroes - Bargat
+//
+//  Created by Thibault Dev on 13/05/2018.
+//  Copyright © 2018 Thibault Dev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
